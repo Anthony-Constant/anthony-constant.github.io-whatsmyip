d12bb2609130c765224eed9693b231f96f6271f5
Anthony Constant <83375598+Anthony-Constant@users.noreply.github.com>
2023-06-08 16:16:58 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -23,19 +23,30 @@
       .catch(error => console.log('Error:', error));
 
      // Copy IP Address Button Function
-      const copyIPButton = document.getElementById('copy-ip-button');
-      const ipAddressElement = document.getElementById('ip-address');
-  
-      copyIPButton.addEventListener('click', () => {
-        const ipAddress = ipAddressElement.innerText;
-  
-        navigator.clipboard.writeText(ipAddress)
-          .then(() => {
-            alert('IP address copied!');
-          })
-          .catch((error) => {
-            console.error('Failed to copy IP address:', error);
-          });
-      });
+     const copyIPButton = document.getElementById('copy-ip-button');
+     const ipAddressElement = document.getElementById('ip-address');
+     
+     copyIPButton.addEventListener('click', () => {
+       const ipAddress = ipAddressElement.innerText;
+     
+       navigator.clipboard.writeText(ipAddress)
+         .then(() => {
+           copyIPButton.textContent = 'Copied';
+           copyIPButton.disabled = true;
+           copyIPButton.className = 'btn btn-success';
+     
+           setTimeout(() => {
+             copyIPButton.textContent = 'Copy IP Address';
+             copyIPButton.disabled = false;
+             copyIPButton.className = 'btn btn-primary';
+           }, 2000); // Reset to original state after 2000 milliseconds (2 seconds)
+         })
+         .catch((error) => {
+           console.error('Failed to copy IP address:', error);
+         });
+     });
+     
+     
+     
 
       
