76e5f90eec188a3de365a9ce93f28d0e557ecd73
Anthony Constant <83375598+Anthony-Constant@users.noreply.github.com>
2023-12-14 18:46:03 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@
      });
      
      
-     if (window.VANTA) window.VANTA.GLOBE({
+     if (window.VANTA) window.VANTA.DOTS({
       el: "html",
       mouseControls: true,
       touchControls: true,
@@ -56,9 +56,15 @@
       minWidth: 200.00,
       scale: 1.00,
       scaleMobile: 1.00,
-       color: 0x3fddff,
-        backgroundColor: 0x101820
-      
+      showLines: false,
+      color: 0x3fddff,
+      color2: 0xff8820,
+      backgroundColor: 0x101820
+    })
+    
+    _strk.push(function() {
+      setVanta()
+      window.edit_page.Event.subscribe( "Page.beforeNewOneFadeIn", setVanta )
     })
 
       
